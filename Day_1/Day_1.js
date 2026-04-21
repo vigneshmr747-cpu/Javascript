23174587b591857172ae5c61901db3a2371232d1
AbishekSathiyan <abidinesh2213.gmail.com>
2026-04-21 14:03:21 +0530

    Vignesh Code Added

---
+++ Day_1/Day_1.js
@@ -0,0 +1,30 @@
+// 🔹 Variables
+let name = "Abishek";   // can change later
+const age = 22;         // fixed value
+var city = "Chennai";   // old method
+
+// 🔹 Printing Output
+console.log("Hello World"); // prints text
+console.log(name);          // prints variable value
+console.log(age);
+
+// 🔹 Datatypes
+let str = "Hello";    // String (text)
+let num = 10;         // Number
+let bool = true;      // Boolean (true/false)
+let x = null;         // Null (empty value)
+let y;                // Undefined (no value assigned)
+
+// 🔹 Array (list of values)
+let arr = [1, 2, 3];
+
+// 🔹 Object (key-value pair)
+let obj = {
+  name: "Abi",
+  age: 22
+};
+
+// 🔹 Print everything
+console.log(str, num, bool, x, y);
+console.log(arr);
+console.log(obj);
